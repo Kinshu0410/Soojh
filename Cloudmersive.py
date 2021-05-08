d37e5554a3853436d7ea19ce7cd655c6a3b422f2
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-05-08 09:14:38 +0530

    Update Cloudmersive.py

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -114,7 +114,7 @@ def poll(update, context):
           allows_multiple_answers=False,
       )
 
-    else if options5 is not None:
+    elif options5 is not None:
       co=int(corr)-1
       message = context.bot.send_poll(
         update.effective_chat.id,
@@ -126,7 +126,7 @@ def poll(update, context):
         is_anonymous=False,
         allows_multiple_answers=False,
     )
-    else if options5 is None:
+    elif if options5 is None:
       co=int(corr)-1
       message = context.bot.send_poll(
         update.effective_chat.id,

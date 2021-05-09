1fb6178a755e9a2dbc02f1d3ea67812dd4505f61
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-05-09 09:34:16 +0530

    Update Cloudmersive.py

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -379,7 +379,7 @@ def receive_poll(update, context):
     question=re.sub("𝗤. ", "", question)
     #question=re.sub(" ■_𝗜𝗺𝗽𝗼𝗿𝘁𝗮𝗻𝘁_𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻_■", "", question)
     q=re.sub("[\[].*?\/.*?[\]] ", "", question)
-    q=re.sub("[\n]{1,}| {1,}", " ", question)
+    q=re.sub("\n{1,}| {1,}", " ", question)
     #q="𝗤. "+qu
     #q=q+"\n\n  ■_𝗜𝗺𝗽𝗼𝗿𝘁𝗮𝗻𝘁_𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻_■"
     options=[o.text for o in actual_poll.options]

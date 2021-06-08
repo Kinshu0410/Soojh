0769fbd1f66dd542c9197d6ecdff2832715d58e7
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-06-08 07:57:41 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -320,6 +320,7 @@ def receive_poll_answer(update,context):
     	try:
 	    	
 	    	ree="yooooooko#"
+	    	print("j = "+str(j))
 	    	print("correct options = "+str(corec))
 	    	if X==j+1:
     			j=0

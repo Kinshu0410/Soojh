9ac4ac3538a9924e79c0572a4697b197253f0c2b
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-06-08 17:59:13 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -373,7 +373,7 @@ def receive_poll_answer(update,context):
 		    			ree=ree+"\n"+<a href="https://t.me/"+Uname>+str(Fname)+</a>+" gain "+str(Rs)+"/"+str(len(db[Textstr0]['que'])*4)+" Marks"
 		    			print(ree)
 		    	yo="🏁 The quiz \'"+Textstr0+"\' has finished!\n\n"+str(len(db[Textstr0]['que']))+" questions answered\n\n"+ree
-		    	context.bot.editMessageText(chat_id=chatid, message_id=mess.message_id, text=yo)
+		    	context.bot.editMessageText(chat_id=chatid, message_id=mess.message_id, text=yo,parse_mode=telegram.ParseMode.HTML))
     			re=""
     			
 

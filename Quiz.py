6f48ca5898b382e8a96c3f45ef0fb1c04f0e657e
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-06-13 07:29:53 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -61,7 +61,7 @@ def send_typing_action(func):
     return command_func
 
 
-LIST_OF_ADMINS = ["Kinbin247","makefuturebright","testing123215","imKkala", "amit_y11", "Gksgj", "ANKITAdidi", "Naaj6", "mr_abhiiii", "Selenasaffron", "Sid000123", "sohitk", "jai_hind_1", "Study_Quiz_India", "GK_Zone", "Maths_Quiz_Notes"]
+LIST_OF_ADMINS = ["Kinbin247"]
 
 def restricted(func):
     @wraps(func)
@@ -741,6 +741,7 @@ def ghn(update,context):
     context.bot.send_message(chat_id=Time1, text=userText)
     return GHN
 
+@restricted
 @run_async
 @send_typing_action
 def poll(update, context):

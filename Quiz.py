877b473bedd6e4d97b0905343ad9d210f1dbf2e1
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-06-13 07:15:03 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -793,8 +793,8 @@ def poll(update, context):
             type=Poll.QUIZ,
             correct_option_id=co,
             explanation=options5,
-            is_anonymous=False,
             is_close=True,
+            is_anonymous=False,
             allows_multiple_answers=False,
         )
         elif options5 =="":
@@ -805,8 +805,8 @@ def poll(update, context):
             options,
             type=Poll.QUIZ,
             correct_option_id=co,#explanation=options5,
-            is_anonymous=False,
             is_close=True,
+            is_anonymous=False,
             allows_multiple_answers=False,
         )
         # Save some info about the poll the bot_data for later use in receive_poll_answer

533e5db201af48d2c1a8d5d337cecb46f1c9133e
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-07-11 09:03:41 +0530

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -2,5 +2,7 @@ from pymongo import MongoClient
 import dns
 str = 'mongodb+srv://Kinshu04101:Qwert123@cluster0.ckcyx.mongodb.net/test?retryWrites=true&w=majority'
 client=MongoClient(str)
-
+db=client["test"]
+col=db["test1"]
+print(db.list_collection_names())
 print("hjjk")

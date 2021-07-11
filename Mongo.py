48a9c304e3f308c47e2e967ca0575f6d21143829
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-07-11 08:27:54 +0530

    Create Mongo.py

---
+++ Mongo.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+import urllib
+str = 'mongodb+srv://Kinshu04101:Qwert123@cluster0.ckcyx.mongodb.net/test?retryWrites=true&w=majority'
+client=MongoClient(str)
+
+print("hjjk")

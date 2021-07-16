9d07d00894745559e55ffb9f0c6b0723d5b9e92d
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-07-16 08:15:21 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -833,7 +833,7 @@ def quizc(update,context):
                     print("payload not done ="+str(e))
             
                     
-            context.bot.send_message(chat_id=channelid, text="Now you can share polls easily first add my bot into your group\n\n                  <a href=\"https://telegram.me/Soojhboojh_01bot?startgroup=true\"> 🌐 Add me ☜ </a>\n\nGive me <b>poll permission</b> for sharing polls.\nIf you are bot administration than give me <b>Media Permission</b> so i send result\nand last give me <b>Delete message permission</b> so when your group members send my command without my permission than there messages will be delete.\n\nUse this command <a href=\"tg://openmessage?user_id=1431722823\">ONLY HERE</a> /pollf", parse_mode=ParseMode.HTML)
+            context.bot.send_message(chat_id=channelid, text="Now you can share polls easily first add my bot into your group\n\n        <a href=\"https://telegram.me/Soojhboojh_01bot?startgroup=true\"> 🌐 Add me ☜ </a>\n\nGive me <b>poll permission</b> for sharing polls.\nIf you are bot administration than give me <b>Media Permission</b> so i send result\nand last give me <b>Delete message permission</b> so when your group members send my command without my permission than there messages will be delete.\n\nUse this command <a href=\"tg://openmessage?user_id=1431722823\">ONLY HERE</a> /pollf", parse_mode=ParseMode.HTML)
 
                 
     

1e4508fd8bcf240abfa4c1d85ba67ab5d3ef7af2
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-07-29 09:28:26 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1079,6 +1079,7 @@ def alarm(context: CallbackContext):
 		            allows_multiple_answers=False,
 		            parse_mode=ParseMode.HTML #,disable_web_page_preview = True
 	    		)
+	    		time.sleep(5)
     	context.job_queue.run_once(alarm, due, context=chat_id, name=str(chat_id))
     	#time.sleep(5)
     except Exception as e:

d09f513ad321123b47670927a71de0c0300ef961
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-08-03 22:33:19 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1166,7 +1166,7 @@ def poll(update, context):
             update.effective_chat.id,
             que,
             options,
-            is_anonymous=False,
+            is_anonymous=True,
             allows_multiple_answers=False,
         )
         elif options5 !="":
@@ -1178,7 +1178,7 @@ def poll(update, context):
             type=Poll.QUIZ,
             correct_option_id=co,
             explanation=options5,
-            is_anonymous=False,
+            is_anonymous=True,
             allows_multiple_answers=False,
         )
         elif options5 =="":
@@ -1189,7 +1189,7 @@ def poll(update, context):
             options,
             type=Poll.QUIZ,
             correct_option_id=co,#explanation=options5,
-            is_anonymous=False,
+            is_anonymous=True,
             allows_multiple_answers=False,
         )
         # Save some info about the poll the bot_data for later use in receive_poll_answer

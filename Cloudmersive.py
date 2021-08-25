0cfdecff9c8dc0c31b2424a70c763f99165ba7a2
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-08-25 16:12:23 +0530

    Update Cloudmersive.py

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -97,9 +97,10 @@ def poll(update, context):
     quest=re.sub(" "," ",quest)
     if 1==1:
         q=quest[0:-1]
-        q=re.sub("(\n| )(\(a\) ){10,}|(\n| |)Sandeep Choudhary(\n| |)|(\n| |)🤗.*?🤗(\n| |)|Poll to Text Bot\:\n|Soojh Boojh Bot - 02\:\n|NaN| Q.*\.|^\. |^\.", "", q)
+        q=re.sub("(\n| )(\(a\) ){10,}|(\n| |)Sandeep Choudhary(\n| |)|(\n| |)🤗.*?🤗(\n| |)|Poll to Text Bot\:\n|Soojh Boojh Bot - 02\:\n|NaN| Q.*\.|^\. |^\.", "\n", q)
         q=re.sub("(\n|\ )( |)(\(|\[|)(A|B|C|D|a|b|c|d|अ|ब|स|द|1|2|3|4|ए|ऐ|बी|सी|डी)(\)|\]|\.)(\.|\ |)", "\n", q)
         q=re.sub("\n{2,}", "\n", q)
+        q=re.sub("^\n", "", q)
 
     
         q=re.split("[\n]", q)

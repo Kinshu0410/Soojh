854860b6de84dfb27b7b5b2e81d96f6bfeddaaa1
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2021-12-16 13:07:53 +0530

    Update Quiz3.py

--- Quiz3.py
+++ Quiz3.py
@@ -852,13 +852,14 @@ def quizc(update,context):
                 ],
             ]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            context.bot.send_message(chat_id=channelid,text='🌐 Click here for Sharing ☜', reply_markup=reply_markup)
+            context.bot.send_message(chat_id=channelid,text='<b>'+userText+'</b>', reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+            context.bot.send_message(chat_id="@SOOJH_BOOJH_BOT_discussion_grouo",text='<b>'+userText+'</b>', reply_markup=reply_markup, parse_mode=ParseMode.HTML)
             
             
             
             
             
-            #context.bot.send_message(chat_id=channelid, text="<a href=\"https://telegram.me/Soojhboojh_01bot?start\">🌐 Click Sharing ☜ </a>", parse_mode=ParseMode.HTML,disable_web_page_preview = True)
+            #context.bot.send_message(chat_id=channelid, text="<a href=\"https://telegram.me/Soojhboojh_01bot?start\">🌐 Click Sharing ☜ </a>", parse_mode=ParseMode.HTML ,disable_web_page_preview = True)
 
                 
 
@@ -1398,7 +1399,7 @@ def pollf(update,context):
                 ],
             ]
                         reply_markup = InlineKeyboardMarkup(keyboard)
-                        context.bot.send_message(chat_id=chat___id,text='🌐 Click here for Sharing ☜', reply_markup=reply_markup)
+                        context.bot.send_message(chat_id=chat___id,text='<b>'+quiz_name+'</b>', reply_markup=reply_markup,parse_mode=ParseMode.HTML)
                         #context.bot.send_message(chat_id=chat___id, text="<a href=\"https://telegram.me/Soojhboojh_01bot?start\">🌐 Click Sharing ☜ </a>", parse_mode=ParseMode.HTML,disable_web_page_preview = True)
                     except Exception as e:
                         context.bot.send_message(chat_id=chat___id, text="Give me Polls send permission to upload quiz hereaaa...\nerror name = "+str(e), parse_mode=ParseMode.HTML)

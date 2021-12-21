234a93d2d7f19d03b004ccaeb747677d822fc7a1
kinshusharm0412 <kinshusharm0412@gmail.com>
2021-12-21 17:20:09 +0530

    exp false to None convert

--- Quiz3.py
+++ Quiz3.py
@@ -244,10 +244,10 @@ def photo(update: Update, _: CallbackContext) -> int:
     if exp:
         if reaaa.findall("@[a-zA-Z0-9_-]",exp):
         	exp=reaaa.sub(" "," ",exp) 
-        	exp=False
+        	exp=None
         elif reaaa.findall("http(s|)://",exp):
         	exp=reaaa.sub(" "," ",exp) 
-        	exp=False
+        	exp=None
     if exp is not None:
         exp= reaaa.sub(r"(#\w*)|(http(s|)://[a-zA-Z0-9_/])", "", exp)
         exp= reaaa.sub(r"(\n| |)join(\n| |)", "", exp)

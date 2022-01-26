61bf437227e1412d92f624d4abedf35a7fd36989
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-01-26 10:02:38 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz3.py
+++ Quiz3.py
@@ -1240,7 +1240,7 @@ def alarm(context: CallbackContext):
 @send_typing_action
 def poll(update, context):
     """Sends a predefined poll"""
-    if re.match("^https://t\.me/QuizBot\?start\=JJGoHW80", update.message.reply_markup.inline_keyboard[0][0].url):
+    if reaaa.match("^https://t\.me/QuizBot\?start\=JJGoHW80", update.message.reply_markup.inline_keyboard[0][0].url):
 	    y=context.bot.get_chat_administrators(chat_id=update.message.chat.id)
 	    xid=[]
 	    for x in y:

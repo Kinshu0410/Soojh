f3bc1db76d1861639943aef16d23e3768045638f
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-01-27 08:20:35 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -32,8 +32,8 @@ async def schedule_job(client:Client,message:Message):
 	name= clientmongo["group_schedule"].list_collection_names()
 	for x in name:
 		try:
-			print(scheduler.add_job(job2, "cron",hour='7-12', minute='58',args=(x,client,message,) ,id="job2"+str(x)))
-			print(scheduler.add_job(job1, "cron", hour='7-23',args=(x,client,message,) ,id="job1"+str(x)))
+			print(scheduler.add_job(job2, "cron",hour='9-23', minute='58',args=(x,client,message,) ,id="job2"+str(x)))
+			print(scheduler.add_job(job1, "cron", hour='10-23',args=(x,client,message,) ,id="job1"+str(x)))
 		except:
 			pass
 	scheduler.start()

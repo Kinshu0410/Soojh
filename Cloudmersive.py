d8015f6dfdac9096cb603b01cda6eadefd0c4931
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-01-30 10:09:59 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -137,6 +137,7 @@ async def job2(x,client:Client,message:Message):
 		else:
 			now=str(int(time.ctime(time.time() +19800)[11:13])+1)+" AM"
 		mass=await app.send_message(int(x)," NEXT QUIZ play in 2 Minutes\n\n"+ col.find_one({"data":{"$type":"array"}})["data"][Nu[0]][list(col.find_one({"data":{"$type":"array"}})["data"][Nu[0]].keys())[0]],disable_web_page_preview=True)
+		await app.send_message(message.chat.id, "0:1:50") 
 	except Exception as e:
 		print("def job2 in cloudmersiver error name = "+str(e))
 
@@ -470,15 +471,17 @@ async def job(client:Client,message:Message):
 		#print(mess1)
 		timer=reaaa.split(":",mess1.text)
 		total=int(timer[0])*3600+int(timer[1])*60+int(timer[2])
-		
-		if total//10>=1:
-			mass=str((total-10)//3600)+":"+str((total-10)//60-((total-10)//3600)*60)+":"+str((total-10)-((total-10)//60-((total-10)//3600)*60)*60-((total-10)//3600)*3600)
-			#mass=str((total-1-x*5)//3600)+":"+str((total-1-x*5)//60-((total-1-x*5)//3600)*60)+":"+str((total-1-x*5)-((total-1-x*5)//60-((total-1-x*5)//3600)*60)*60-((total-1-x*5)//3600)*3600)
-			await app.edit_message_text(message.chat.id, message.message_id,mass)
+		try:
+			if total//10>=1:
+				mass=str((total-10)//3600)+":"+str((total-10)//60-((total-10)//3600)*60)+":"+str((total-10)-((total-10)//60-((total-10)//3600)*60)*60-((total-10)//3600)*3600)
+				#mass=str((total-1-x*5)//3600)+":"+str((total-1-x*5)//60-((total-1-x*5)//3600)*60)+":"+str((total-1-x*5)-((total-1-x*5)//60-((total-1-x*5)//3600)*60)*60-((total-1-x*5)//3600)*3600)
+				await app.edit_message_text(message.chat.id, message.message_id,mass)
+				
+			else:
+				await app.edit_message_text(message.chat.id, message.message_id,"times up Stop! Stop! Stop!")
+		except:
+			await app.edit_message_text(message.chat.id, message.message_id,"Some error comes...")
 			
-		else:
-			await app.edit_message_text(message.chat.id, message.message_id,"times up Stop! Stop! Stop!")
-			scheduler.shutdown()
 			
 			
 		

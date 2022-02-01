17e33047980468327bf5c95baa392c882e79f75d
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-02-01 14:02:39 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -137,8 +137,8 @@ async def job2(x,client:Client,message:Message):
 		else:
 			now=str(int(time.ctime(time.time() +19800)[11:13])+1)+" AM"
 		await app.send_message(int(x),"💬 NEXT QUIZ play in 2 Minutes\n\n"+ col.find_one({"data":{"$type":"array"}})["data"][Nu[0]][list(col.find_one({"data":{"$type":"array"}})["data"][Nu[0]].keys())[0]],disable_web_page_preview=True)
-		time.sleep(5)
-		mass=await app.send_message(int(x), "0:1:50")
+		time.sleep(10)
+		mass=await app.send_message(int(x), "0:1:30")
 		scheduler.add_job(job3, "interval", seconds=10,replace_existing=True,args=(mass,client,message,) ,id="job3"+str(x))
 		print("job3 added for = "+str(message.first_name))
 	except Exception as e:

d28922e13b8cbce071dc26b2dde9f6432fc19a0f
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-02-01 07:14:45 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz3.py
+++ Quiz3.py
@@ -1942,7 +1942,7 @@ def current(update,context):
     #print(URL1)
     for x in URL1:
     	print(x)
-    	if x.startswith("https://soojhboojhquiz.blogspot.com/2022/01"):
+    	if x.startswith("https://soojhboojhquiz.blogspot.com/2022/02"):
     	    URL.append(x)
     	    
     	

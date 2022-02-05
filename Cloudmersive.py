e58bcf5252b654c7d5b4adb43d1c0e4b238d5c26
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-02-05 10:15:43 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -39,7 +39,10 @@ async def job2_partener(client:Client,message:Message):
 			    for zz in name1:
 			        
 			        scheduler.add_job(job4, "cron", hour="12",minute="5-12",replace_existing=True,args=(zz,client,message,) ,id="job4"+str(zz))
-			        #scheduler.start()
+			        try:
+			            scheduler.start()
+			        except:
+			            pass
 			    col=clientmongo["group_schedule"][str(message.chat.id)]
 			    Nu=col.find_one({"Nu":{"$type":"array"}})["Nu"]
 			    hour1=col.find_one({"Time":{"$type":"string"}})["Time"]

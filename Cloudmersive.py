81de48d60488b0c245467484962fcc956bf7572a
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-02-10 08:17:21 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -162,12 +162,13 @@ async def job2(x,client:Client,message:Message):
 @app.on_message(filters.regex("^\d{1,}-\d{1,}$") )#& filters.incoming)
 async def job2_partener(client:Client,message:Message):
     	xx=reaaa.split("-",message.text)
+    	mess=None
     	for x in range(int(xx[0]),int(xx[1])+1):
     		try:
         		try:
         		    mess=(await client.vote_poll(chat_id=message.chat.id, message_id=x,options=1))
         		except:
-        		    print(functions.messages.GetPollVotes(peer= await app.resolve_peer(-1001495791558),id=int(11127),limit=10))#mess=message.poll
+        		    print(await app.send(functions.messages.GetPollVotes(peer= await app.resolve_peer(-1001495791558),id=int(11127),limit=10)))#mess=message.poll
         		await app.send_message(message.chat.id, await app.send(functions.messages.GetPollVotes(peer= await app.resolve_peer(-1001495791558),id=int(11127),limit=10)))
     		except Exception as e:
     		    print(message.chat.id)

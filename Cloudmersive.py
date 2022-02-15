ae7cf1e4e76e169e6f7f487b7e35ecb3b0f72b27
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-02-15 16:21:23 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -244,43 +244,42 @@ async def job2_partene(client:Client,message:Message):
 	fname=id_generator()
 	z=1
 	#r = requests.get(xx)
-	chunk_size = 1000
+	chunk_size = 10000
 	r = requests.get(xx, stream=True)
-	f=open(fname+"mp4", "wb")
-	try:
+	with open(fname+".mp4", "wb") as f:
 		#print("in file")
 		for chunk in r.iter_content(chunk_size=chunk_size):
 			f.write(chunk)
 			z+=1
-			if z==1000:
+			if z==100:
 				break
-			#print("file created")
-	#f = open(fname+".MP4",'wb')
-	#f.write(r.content)
-	except:
-		pass
+	
 	try:
 		await app.send_document(message.chat.id, fname+".mp4")
 	except Exception as e:
 		await app.send_message(message.chat.id,str(e))
 	os.remove(fname+".mp4")
-	
+
 @app.on_message(filters.regex("^Z") & filters.outgoing)
-async def job2_partene(client:Client,message:Message):
+async def job2_hhpartene(client:Client,message:Message):
 	xx=reaaa.sub("^Z","",message.text)
 	fname=id_generator()
 	z=1
 	#r = requests.get(xx)
-	f=open(fname+"mp4", "wb")
 	r = requests.get(xx)
-	f.write(r.content)
+	with open(fname+".mp4", "wb") as f:
+		#print("in file")
+		f.write(r.content)
+			z+=1
+			if z==100:
+				break
+	
 	try:
 		await app.send_document(message.chat.id, fname+".mp4")
 	except Exception as e:
 		await app.send_message(message.chat.id,str(e))
 	os.remove(fname+".mp4")
-	
-	
+
 @app.on_message(filters.regex("^\d{1,}-\d{1,}$") )#& filters.incoming)
 async def job2_partener1(client:Client,message:Message):
         xx=reaaa.split("-",message.text)

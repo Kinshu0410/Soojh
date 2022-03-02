ea50612a3fac4535548e0935112bae4a31eeb070
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-03-02 17:32:44 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz3.py
+++ Quiz3.py
@@ -1146,62 +1146,72 @@ Man=[711296045,1001183009,776365745,1527108544,2020953330,1202919365,1309577346,
 Group=[-1001517843177,-1001183315065,-1001293483771,-1001362563196,-1001307100573,-1001187254179,-1001368097755,-1001222891254,-1001164423875,-1001487436278,-1001428838285,-1001664461759,-1001664461759,-1001415742406,-1001725784523]
 @restricted
 def ghppp1(update,context):
-    cid=""
-    id=""
-    for li1 in range(len(Man)):
-        if str(update.message.chat.id) in str(Man[li1]):
-            cid=Group[li1]
-            id=Man[li1]
-    userText=update.message.poll
-    context.bot.send_message(chat_id=-1001539629311, text="<a href=\"tg://openmessage?user_id="+str(cid)+"\">chat info</a>\n<a href=\"tg://openmessage?user_id="+str(id)+"\">grouo</a>",parse_mode=ParseMode.HTML,disable_web_page_preview = True)
-    print("ghn1 started")
-    que=userText.question
-    que=reaaa.sub(" "," ",que)
-    que=reaaa.sub("^(☞( ){1,}|(((\[\d{1,}/\d{1,}\] ){1,}|)(Q_\. |Q_\.|Q_ |Q_|Q\. |Q\.|Q |Q|)))","",que)
-    que=reaaa.sub("(\n| |)✍{0,} Priti Gupta ✍{0,}(\n| |)","",que)
-    que=reaaa.sub("(\n| |)Sandeep Choudhary(\n| |)","",que)
-    que=reaaa.sub("(\n| |)🤗.*?🤗(\n| |)","",que)
-    que=reaaa.sub("(\n){1,}","\n",que)
-    que=reaaa.sub("^\n","",que)
-    que= reaaa.sub(r"(@\w*)|(http(s|)://[a-zA-Z0-9_/\.])", "", que)
-    options=[o.text for o in userText.options]
-    for yx in range(len(options)):
-        options[yx]=reaaa.sub(" "," ",options[yx])
-    co=userText.correct_option_id
-    explan=userText.explanation
-
-    if explan:
-        explan=reaaa.sub(" "," ",explan)
-        if reaaa.findall("@[a-zA-Z0-9_-]",explan):
-            explan=None
-        elif reaaa.findall("t\.me",explan):
-            explan=None
-        elif reaaa.findall("^\d{8,}$",explan):
-            explan=None
-
-    print("poll")
-    context.bot.send_poll(
-            chat_id=int(cid),
-            question=que,
-            options=options,
-            type=Poll.QUIZ,
-            correct_option_id=co,
-            explanation=explan,
-            is_anonymous=True,
-            allows_multiple_answers=False,
-            parse_mode=ParseMode.HTML)
-    context.bot.send_poll(
-            chat_id=int(-1001539629311),
-            question=que,
-            options=options,
-            type=Poll.QUIZ,
-            correct_option_id=co,
-            explanation=explan,
-            is_anonymous=True,
-            allows_multiple_answers=False,
-            parse_mode=ParseMode.HTML)
-    time.sleep(5)
-    #return ConversationHandler.END
+    if update.message.chat.id==-1001682640576:
+	    if update.message.from_user.id==711296045:
+	        
+	        ali=[-1001487436278,-1001135796728,-1001443924980,
+-1001472989895,-1001222891254,-1001517843177,-1001478660095,-1001173492501,-1001177789955,-1001342905358,-1001725784523,-1001664461759,-1001412214082,-1001412214082,-1001244305820,-1001393712887]
+	        [ali.append(x["Uid"]) for x in client["user"]["sub"].find({})]
+	        for x in ali:
+	            context.bot.forward_message(chat_id=x,from_chat_id=update.message.chat_id,message_id=update.message.message_id)
+	        time.sleep(5)
+    else:
+        cid=""
+        id=""
+        for li1 in range(len(Man)):
+            if str(update.message.chat.id) in str(Man[li1]):
+                cid=Group[li1]
+                id=Man[li1]
+        userText=update.message.poll
+        context.bot.send_message(chat_id=-1001539629311, text="<a href=\"tg://openmessage?user_id="+str(cid)+"\">chat info</a>\n<a href=\"tg://openmessage?user_id="+str(id)+"\">grouo</a>",parse_mode=ParseMode.HTML,disable_web_page_preview = True)
+        print("ghn1 started")
+        que=userText.question
+        que=reaaa.sub(" "," ",que)
+        que=reaaa.sub("^(☞( ){1,}|(((\[\d{1,}/\d{1,}\] ){1,}|)(Q_\. |Q_\.|Q_ |Q_|Q\. |Q\.|Q |Q|)))","",que)
+        que=reaaa.sub("(\n| |)✍{0,} Priti Gupta ✍{0,}(\n| |)","",que)
+        que=reaaa.sub("(\n| |)Sandeep Choudhary(\n| |)","",que)
+        que=reaaa.sub("(\n| |)🤗.*?🤗(\n| |)","",que)
+        que=reaaa.sub("(\n){1,}","\n",que)
+        que=reaaa.sub("^\n","",que)
+        que= reaaa.sub(r"(@\w*)|(http(s|)://[a-zA-Z0-9_/\.])", "", que)
+        options=[o.text for o in userText.options]
+        for yx in range(len(options)):
+            options[yx]=reaaa.sub(" "," ",options[yx])
+        co=userText.correct_option_id
+        explan=userText.explanation
+    
+        if explan:
+            explan=reaaa.sub(" "," ",explan)
+            if reaaa.findall("@[a-zA-Z0-9_-]",explan):
+                explan=None
+            elif reaaa.findall("t\.me",explan):
+                explan=None
+            elif reaaa.findall("^\d{8,}$",explan):
+                explan=None
+    
+        print("poll")
+        context.bot.send_poll(
+                chat_id=int(cid),
+                question=que,
+                options=options,
+                type=Poll.QUIZ,
+                correct_option_id=co,
+                explanation=explan,
+                is_anonymous=True,
+                allows_multiple_answers=False,
+                parse_mode=ParseMode.HTML)
+        context.bot.send_poll(
+                chat_id=int(-1001539629311),
+                question=que,
+                options=options,
+                type=Poll.QUIZ,
+                correct_option_id=co,
+                explanation=explan,
+                is_anonymous=True,
+                allows_multiple_answers=False,
+                parse_mode=ParseMode.HTML)
+        time.sleep(5)
+        #return ConversationHandler.END
 x=0
 admin2=[]
 def ghn2(update,context):

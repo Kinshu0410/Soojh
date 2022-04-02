b761dad871c031068f1975642f221af1a1e7ac8f
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-04-02 09:05:48 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz3.py
+++ Quiz3.py
@@ -1467,7 +1467,8 @@ def poll(update, context):
 	            for y in range(len(data[x])):
 	                worksheet.write(x,y, data[x][y])
 	        workbook.close()
-	        context.bot.send_document(update.effective_chat.id, open('Result.xlsx', "rb"))#,caption=caption1, parse_mode=ParseMode.HTML,reply_to_message_id=colmessage)
+	        context.bot.send_message(chat_id=update.message.chat.id, text="Quiz added") 
+	        context.bot.send_document(chat_id=update.message.chat.id, open('Result.xlsx', "rb"))#,caption=caption1, parse_mode=ParseMode.HTML,reply_to_message_id=colmessage)
 	    except:
 	        pass
 	    quest=(update.message.text)

cf4e12c0cac120212c86ad627e9212484609045c
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-04-02 09:25:41 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz3.py
+++ Quiz3.py
@@ -1458,6 +1458,7 @@ def poll(update, context):
 		    	time.sleep(5)
     elif str(update.message.from_user.id) in LIST_OF_ADMINS:
 	    try:
+	        context.bot.send_message(chat_id=update.message.chat.id, text="Quiz added")
 	        from excle_c import main
 	        data=main(update.message.text)
 	        import xlsxwriter
@@ -1467,7 +1468,7 @@ def poll(update, context):
 	            for y in range(len(data[x])):
 	                worksheet.write(x,y, data[x][y])
 	        workbook.close()
-	        context.bot.send_message(chat_id=update.message.chat.id, text="Quiz added")
+	        
 	        context.bot.send_document(update.message.chat.id, open('Result.xlsx', "rb"))#,caption=caption1, parse_mode=ParseMode.HTML,reply_to_message_id=colmessage)
 	    except:
 	        pass

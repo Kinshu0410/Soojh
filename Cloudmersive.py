f7a0caf1bf06c81e52fb2c2f361d66ca78c109d7
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-05-05 19:37:31 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -623,18 +623,22 @@ async def job2_partener2(client:Client,message:Message):
             	question=reaaa.sub(r"", "", question)
             	options=[o.text for o in mess1.options]
             	lis=[]
+            	print(explanation)
             	for x in range(len(options)):
             	    options[x]=reaaa.sub("^(\[|\(|)(a|b|c|d|A|B|C|D|E|F|e|f)(\]|\)|)(\. |\.|)","",options[x])
             	    lis.append(x)
+            	print(explanation)
             	#await app.send_message(message.chat.id,str(lis))
             	random.shuffle(lis)
             	#await app.send_message(message.chat.id,str(lis))
+            	print(explanation)
             	correct_option_id = 0
             	for i in range(len(mess1.options)):
             	    if mess1.options[i].correct[0]:
             	        correct_option_id = i
             	        break
             	
+            	print(explanation)
             	for i in range(len(lis)):
             	    if lis[i]==correct_option_id:
             	        correct_option_id = i

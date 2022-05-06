80bd66aa93862c5d84f48a4b36c43d2848a3835e
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-05-06 17:41:58 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -645,7 +645,7 @@ async def job2_partener2(client:Client,message:Message):
             	        break
             	options=[options[op] for op in lis]
             	
-            	for o in range(len(options)):
+            	for o in range(len(options)):#
             	    options[o]=bytes('(\\u004'+str(o+1), 'utf-8').decode('unicode-escape')+") "+options[o]#
             	mess2=(await app.send_poll(chat_id=message.chat.id,question="Q "+str(int(xx[2])-nn+1)+". "+question,options=options,correct_option_id =correct_option_id,is_anonymous=False,type=PollType.QUIZ,open_period=tt1,explanation=explanation))
             	

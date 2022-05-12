ed4c559ff5ae140c21737fb6ae27248b6878ce4a
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-05-12 18:45:39 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -794,7 +794,7 @@ async def job2_partener2(client:Client,message:Message):
                     count=count+2
                 zz-=1
                 
-                body={"requests":[{"insertText":{"location":{"index":count},"text":re.sub("([^\u0000-\u05C0\u2100-\u214F\u0900-\u097F])","", str(new_result[x][y]))},},],}
+                body={"requests":[{"insertText":{"location":{"index":count},"text":reaaa.sub("([^\u0000-\u05C0\u2100-\u214F\u0900-\u097F])","", str(new_result[x][y]))},},],}
                 count=count+len(str(new_result[x][y]))
                 print(len(str(new_result[x][y])))
                 print(body)

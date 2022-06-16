4fff63a18a37900b0ce9c2afb8b680384a474e15
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-06-16 22:25:23 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -996,9 +996,9 @@ async def forworhd(client:Client,message:Message):
     		pass
     	else:
     		await app.delete_messages(chat_id=message.chat.id, message_ids=message.id)
-    		mess=(await app.send_message(chat_id=message.chat.id, text=str("you can send poll and photo \n\nThis message delete in 10 seconds")))
-    		await asyncio.sleep(9)
-    		await app.delete_messages(chat_id=message.chat.id, message_ids=mess.id)
+    		#mess=(await app.send_message(chat_id=message.chat.id, text=str("you can send poll and photo \n\nThis message delete in 10 seconds")))
+    		#await asyncio.sleep(9)
+    		#await app.delete_messages(chat_id=message.chat.id, message_ids=mess.id)
     except:
         pass
 

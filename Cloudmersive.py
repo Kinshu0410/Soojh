378ddc6491e4ca63e241132f7591e9a2a3d1d76d
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-06-20 16:14:10 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -998,7 +998,8 @@ async def stop_quiz(client:Client,message:Message):
 			await app.send_message(message.chat.id, '/stop@QuizBot')
 	except:
 		pass
-@app.on_message(filters.regex("^P(1|2|3|4){1,}$" & filters.chat(-1001551893872) )#& filters.incoming)
+
+@app.on_message(filters.regex("^P(1|2|3|4){1,}$") & filters.chat(-1001551893872) )#& filters.incoming)
 async def forworhd1(client:Client,message:Message):
     try:
     	t=reaaa.sub("^P","",message)

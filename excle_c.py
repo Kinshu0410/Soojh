c570f7e2ce01823ec09d04439e65ae355c76ac6d
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-07-09 12:22:27 +0530

    speed se push kiya jyada koi kaam nahi hai

--- excle_c.py
+++ excle_c.py
@@ -64,8 +64,9 @@ def main(y):
         result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                     range=SAMPLE_RANGE_NAME).execute()
         values = result.get('values', [])
-        print(result)
-
+        service = build('drive', 'v3', credentials=creds)
+        req = service.files().export_media(fileId=SAMPLE_SPREADSHEET_ID,mimeType='application/pdf')
+        print(req)
         if not values:
             print('No data found.')
             return

35e53f4534f1b559390562c26f2204056449c780
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-08-08 17:12:43 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -556,7 +556,7 @@ async def job2_partener1(client:Client,message:Message):
         from quickstart import Drive_OCR
         body = {"title": 'Result.pdf'}
         id=Drive_OCR(body).create()
-        replies=Drive_OCR({ "requests": [ { "updateDocumentStyle": { "documentStyle": { "background":{"color":{"color": {"rgbColor": {"red": 1,"green": 1,"blue": 1}}}},"marginTop": { "magnitude": 10, "unit": "PT" },"marginBottom": { "magnitude": 10, "unit": "PT" },"marginLeft": { "magnitude": 10, "unit": "PT" }, "marginRight": { "magnitude": 10, "unit": "PT" },"marginHeader": { "magnitude":10, "unit": "PT" },"marginFooter": { "magnitude": 0, "unit": "PT" },"pageSize": {"height":{"magnitude": 1000, "unit": "PT"},"width":{"magnitude": 500, "unit": "PT"}}}, "fields": "*" } }] } ).update(id)
+        
         mess1="vote alreddy given"
         result={}
         new_result = {}
@@ -744,7 +744,7 @@ async def job2_partener2(client:Client,message:Message):
         body = {"title": 'Result.pdf'}
         id=Drive_OCR(body).create()
         yy=None
-        
+        replies=Drive_OCR({ "requests": [ { "updateDocumentStyle": { "documentStyle": { "background":{"color":{"color": {"rgbColor": {"red": 1,"green": 1,"blue": 1}}}},"marginTop": { "magnitude": 10, "unit": "PT" },"marginBottom": { "magnitude": 10, "unit": "PT" },"marginLeft": { "magnitude": 10, "unit": "PT" }, "marginRight": { "magnitude": 10, "unit": "PT" },"marginHeader": { "magnitude":10, "unit": "PT" },"marginFooter": { "magnitude": 0, "unit": "PT" },"pageSize": {"height":{"magnitude": 1000, "unit": "PT"},"width":{"magnitude": 500, "unit": "PT"}}}, "fields": "*" } }] } ).update(id)
         print(xx)
         li=[x for x in range(int(xx[1]),int(xx[1])+int(xx[2]))]
         #random.shuffle(li)

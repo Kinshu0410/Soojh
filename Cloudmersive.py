152d34e56aa61ce2bea022e80a58a2c776d9a98e
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-08-09 23:43:24 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -998,7 +998,7 @@ async def job2_partener2(client:Client,message:Message):
             Drive_OCR(body).download(id)
             from PyPDF2 import PdfFileReader, PdfFileWriter 
             pdf_file_path = 'Result.pdf' 
-            file_base_name = "Question Bank"
+            file_base_name = "Question_Bank"
             pdf = PdfFileReader(pdf_file_path) 
             pages = [0] # page 1, 3, 5 
             pdfWriter = PdfFileWriter() 

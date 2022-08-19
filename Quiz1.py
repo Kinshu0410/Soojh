05eb468e28fca5ad714c6c9c617bf6021a3eff6a
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2022-08-19 19:49:34 +0530

    Update Quiz1.py

--- Quiz1.py
+++ Quiz1.py
@@ -2569,7 +2569,7 @@ def done(update,context):
     	item=[{"updateSettings":{"settings":{"quizSettings":{"isQuiz":True}},"updateMask":"quizSettings.isQuiz"}}]
     	Drive_OCR(item).google_form_update(id=id)
     	pack=data[update.message.from_user.id]["pack"]
-    	item=[{"createItem":{"item": {"title": "Your Name","questionItem": {"question": {"required": True,"grading": {"pointValue": 0},"textQuestion":{"paragraph": False}}}},"location": {"index":0}}}]
+    	item=[{"createItem":{"item": {"title": "Your Name","questionItem": {"question": {"required": True,"textQuestion":{"paragraph": False}}}},"location": {"index":0}}}]
     	Drive_OCR(item).google_form_update(id=id)
     	for x in range(len(pack)):
     	    x+=1

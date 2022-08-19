1febc1e9602986ce7b845a84e2dea10c62a73ba7
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-08-19 06:24:13 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -2544,7 +2544,7 @@ def done(update,context):
     try:
     	from quickstart import Drive_OCR
     	#update.message.reply_text(str("ffffffff"))
-    	update.message.reply_text(str(data[update.message.from_user.id]["title"]))
+    	#update.message.reply_text(str(data[update.message.from_user.id]["title"]))
     	id=Drive_OCR({"title":data[update.message.from_user.id]["title"],"document_title": data[update.message.from_user.id]["title"]} ).google_form_create()
     	update.message.reply_text(str(id))
     	update.message.reply_text(id["responderUri"])

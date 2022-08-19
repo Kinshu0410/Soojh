dd38524ed1a71e4d1c6073d06bb4a3307212b594
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2022-08-19 12:38:50 +0530

    Update Quiz1.py

--- Quiz1.py
+++ Quiz1.py
@@ -2482,7 +2482,7 @@ def call8(update,context):
 def gfm(update,context):
     global data
     info1 = update.message.text
-    context.bot.send_message(chat_id=update.message.chat.id,text="title:\ndescription: ")
+    context.bot.send_message(chat_id=update.message.chat.id,text="use /page_braker")
     data[update.message.from_user.id]={}
     data[update.message.from_user.id]["title"]=reaaa.split("\n",info1)[0]
     data[update.message.from_user.id]["description"]="\n".join(reaaa.split("\n",info1)[1:])

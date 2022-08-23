d88f7ce90031c6e9ad3a8bbef7f5a02ff4be5a44
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-08-23 11:03:05 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -2556,7 +2556,7 @@ def done(update,context):
     	#update.message.reply_text(str(id))
     	update.message.reply_text(id["responderUri"])
     	id=id["formId"]
-    	item=[{"updateFormInfo":{"info":{"discription":data[update.message.from_user.id]["description"]},"updateMask":"*"},"updateSettings":{"settings":{"quizSettings":{"isQuiz":True}},"updateMask":"quizSettings.isQuiz"},}]
+    	#item=[{"updateFormInfo":{"info":{"discription":data[update.message.from_user.id]["description"]},"updateMask":"*"},"updateSettings":{"settings":{"quizSettings":{"isQuiz":True}},"updateMask":"quizSettings.isQuiz"},}]
     	item=[{"updateSettings":{"settings":{"quizSettings":{"isQuiz":True}},"updateMask":"quizSettings.isQuiz"},}]
     	Drive_OCR(item).google_form_update(id=id)
     	pack=data[update.message.from_user.id]["pack"]

ea20c240a7f869958852314837e8b870bdb7b0a5
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-09-04 09:18:40 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -186,6 +186,15 @@ async def setting_time(client:Client,message:Message):
 		await app.send_message(int(message.chat.id),"Schedule Reset")
 	scheduler.start()
 
+@app.on_message(filters.text & filters.outgoing & filters.chat(-1001507348250))
+async def text_delete_quizbot(client:Client,message:Message):
+	if bool(reaaa.search("Congratulations to the winners!|Get ready for the quiz|",message.text)):
+		pass
+	elif message.from_user.id==983000232:
+		await app.delete_messages(chat_id=message.chat.id, message_id=message.message_id)		
+		
+
+
 @app.on_message(filters.text & filters.outgoing & filters.chat("TempMail_org_bot"))
 async def job_ads(client:Client,message:Message):
 	xx=message.text

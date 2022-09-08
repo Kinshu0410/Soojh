ff8f128e53cd94714ed5adb30519789e25d428ef
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-09-08 17:20:44 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -1845,6 +1845,7 @@ function d() {
     	    from google_form import main_run,main4
     	    main4(x)
     	    main_run('d')
+    	    context.bot.send_message(chat_id=update.message.chat.id, text=(x))
     	    context.bot.send_message(chat_id=update.message.chat.id, text=(mem.user.id))
     	    context.bot.send_message(chat_id=update.message.chat.id, text=("👆Your Password"))
     	

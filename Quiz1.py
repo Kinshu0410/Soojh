2ffd3a91d4727ad68014d460d645ab8df10e32a4
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-09-08 07:47:11 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -1817,7 +1817,7 @@ def pollf(update,context):
     elif update.message.text.startswith("/start g_f"):
     	text=reaaa.sub("/start g_f","",update.message.text)
     	print(text)
-    	text=reaaa.splite("&id=",text)
+    	text=reaaa.split("&id=",text)
     	mem=get_chat_member("@"+text[0],update.message.id)
     	context.bot.send_message(chat_id=-611934865, text=str(mem))
     	

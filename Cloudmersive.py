ea635f793c71b8e013ca1f84298bf60ea4140ec4
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-09-28 23:57:41 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -619,7 +619,7 @@ async def job2_partener1(client:Client,message:Message):
             	except:
             		mess1=await app.get_messages(message.chat.id,x)
             		mess1=mess1.poll
-            	print(mess1.total_voter_count)
+            	print(mess1)
             	off_set=None
             	for xxxx in range(mess1.total_voter_count//50+1):
             		mess2=await app.invoke(functions.messages.GetPollVotes(peer=await app.resolve_peer(message.chat.id),id=x,limit=mess1.total_voter_count,offset=off_set))

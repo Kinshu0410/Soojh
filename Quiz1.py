25471a81cd56363aab851375672d371211cdc8b6
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-03 12:35:27 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -697,7 +697,7 @@ def uploadfile(update,context):
     update.message.reply_text("send me file.")
     return UPLOAD
 
-from pyrogram import Client
+from pyrogram import Client, idle
 app = Client("my_live_bot",#session_string="BQDQx-MAAcKa6bmK3-vwhmKd0v3v4-SXoQ7PWIIqkl6-0j_96Gq6apAJ1vRFPUQrWwbcHoNLj0ouYgn5yCAvkT-BW8iE-1lYomM1VwAzEKHNuUVqTYrFDpCsAZE2ko1DudXnRUWz1SBkxk8f6JzrDS57sBmx2oEgUBXfiyGjJXJYn2KC-TsCao4Cbt-x6pE3LWwPprjAqsN6LHY2y2WA4QnQVagTclIrp5_Cc4FZbRnyNcL_MwQ-7hLF_5psbi4c1hXOYXYCnjx3KQ0Q--f0ISiGSt8h3xRu39RozHP1ANrB3pz4e_Unmoe8ad7hmhzwuil8uVqaV1qspejkWDo_3cyyadzV2QAAAAAqZYQtAA",
 bot_token="1877489613:AAEWv36y-bbUjQPCemmJ53vSADAgKZB1A-U",
 api_id="13682659",
@@ -708,7 +708,7 @@ async def get_mess_py(x,y):
 	try:
 		await app.start()
 		return_mess=await app.get_messages(x,int(y))
-		await asyncio.sleep(5)
+		await idle()
 		await app.stop()
 		return return_mess.text
 	except:

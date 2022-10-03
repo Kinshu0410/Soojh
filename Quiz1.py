6e45ee1f37e72aa9c8f4869872252e3c1b0e14d2
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-03 09:40:08 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -702,12 +702,12 @@ app = Client("my_live_bot",#session_string="BQDQx-MAAcKa6bmK3-vwhmKd0v3v4-SXoQ7P
 bot_token="1877489613:AAEWv36y-bbUjQPCemmJ53vSADAgKZB1A-U",
 api_id="13682659",
 api_hash="b984d240c5258407ea911f042c9d75f6")
-def get_mess_py(x,y):
+async def get_mess_py(x,y):
 	try:
-		app.start()
-		return_mess=app.get_messages(x,int(y))
+		await app.start()
+		return_mess=await app.get_messages(x,int(y))
 	except:
-		app.stop()
+		await app.stop()
 
 	
 	

2447f7d0c29cb0a9709e986f40e8b85d762918ba
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-04 17:59:10 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -1419,7 +1419,7 @@ def poll(update, context):
     	text=reaaa.sub("https://t.me/","",update.message.text)
     	text=reaaa.split(":|/",text)
     	keyboard=[]
-    	for x in range(len(text[2:])/2):
+    	for x in range(len(text[2:])//2):
     	    mes=check_mess(app.get_messages(text[2*x+2],int(text[2*x+3])).text,[])
     	    keyboard.append([InlineKeyboardButton(str(z+1),callback_data="Link"+str(text[2*x+3])+"_"+str(len(mes))+"_"+str(z+1)+"_"+str(text[2*x+1])) for z in range(len(mes))])
     	    

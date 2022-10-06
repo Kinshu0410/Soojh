b277d4302021d8f3ecd8c250541f896bb9f438b6
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-07 00:10:21 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -2217,16 +2217,33 @@ def button(update: Update, context: CallbackContext) -> None:
 	           
 	       else:
 	           keyboard=[]
+	           
+	           num=1
 	           for y in query.message.reply_markup.inline_keyboard:
 	               print(y)
 	               zz=reaaa.split("_",y[0].callback_data[4:])
 	               try:
 	                   mes=check_mess(get_mess_py(zz[3],int(zz[0])).text,[])
 	                   keyboard1=([InlineKeyboardButton(str("⚙️"),callback_data="Link"+str(zz[0])+"_"+str(len(mes))+"_"+str(0)+"_"+str(zz[3]))])
-	                   keyboard.append(keyboard1+[InlineKeyboardButton(str(z+1),callback_data="Link"+str(zz[0])+"_"+str(len(mes))+"_"+str(z+1)+"_"+str(zz[3])) for z in range(len(mes))])
-	               except:
-	                   pass
-	           
+	                   keyboard2=[]
+	                   for z in range(len(mes)//5):
+	                       keyboard2=[]
+	                       for yy in range(5):
+	                           keyboard2.append(InlineKeyboardButton(str(num),callback_data="Link"+str(zz[0])+"_"+str(len(mes))+"_"+str(num)+"_"+str(zz[3])))
+	                           num+=1
+	                       keyboard.append(keyboard2)
+	                   
+	                   print(str(keyboard))
+	                   keyboard2=[]
+	                   for z in range(len(mes)%5):
+	                       keyboard2.append(InlineKeyboardButton(str(num),callback_data="Link"+str(zz[0])+"_"+str(len(mes))+"_"+str(num)+"_"+str(zz[3])))
+	                       num+=1
+	                   keyboard.append(keyboard2+keyboard1)
+	                   print(str(keyboard))
+	                   #keyboard.append(keyboard3)
+	               except Exception as p:
+	                   
+	                   print(str(keyboard))
 	           reply_markup = InlineKeyboardMarkup(keyboard)
 	           if keyboard==[]:
 	               reply_markup=None

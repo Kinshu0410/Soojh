4632d627a1d263d18737041833e2f66513d4030b
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-07 19:00:49 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -2111,7 +2111,7 @@ def pdfc(update,context):
 	
 from pyrogram import Client, idle
 app = Client("my_live_bot",#session_string="BQDQx-MAAcKa6bmK3-vwhmKd0v3v4-SXoQ7PWIIqkl6-0j_96Gq6apAJ1vRFPUQrWwbcHoNLj0ouYgn5yCAvkT-BW8iE-1lYomM1VwAzEKHNuUVqTYrFDpCsAZE2ko1DudXnRUWz1SBkxk8f6JzrDS57sBmx2oEgUBXfiyGjJXJYn2KC-TsCao4Cbt-x6pE3LWwPprjAqsN6LHY2y2WA4QnQVagTclIrp5_Cc4FZbRnyNcL_MwQ-7hLF_5psbi4c1hXOYXYCnjx3KQ0Q--f0ISiGSt8h3xRu39RozHP1ANrB3pz4e_Unmoe8ad7hmhzwuil8uVqaV1qspejkWDo_3cyyadzV2QAAAAAqZYQtAA",
-bot_token="1877489613:AAEWv36y-bbUjQPCemmJ53vSADAgKZB1A-U",
+bot_token="1431722823:AAHk_VOD0WgepQ1us7eucQm3UQRYacHzmQM",
 api_id="13682659",
 api_hash="b984d240c5258407ea911f042c9d75f6")
 import re as reaaa

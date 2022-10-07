38122ab7513003fb2f077609a4a341941b55e320
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-07 11:24:00 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -1422,20 +1422,20 @@ def poll(update, context):
     	num=1
     	for x in range(len(text[2:])//2):
     	    try:
-	                   mes=check_mess(get_mess_py("@"+text[2*x+1],int(text[2*x+2])).text,[])
-	                   keyboard1=([InlineKeyboardButton(str("⚙️"),callback_data="Link"+str(text[2*x+2])+"_"+str(len(mes))+"_"+str(0)+"_"+str(text[2*x+1]))])
+	                   mes=check_mess(get_mess_py("@"+text[2*x+3],int(text[2*x+4])).text,[])
+	                   keyboard1=([InlineKeyboardButton(str("⚙️"),callback_data="Link"+str(text[2*x+4])+"_"+str(len(mes))+"_"+str(0)+"_"+str(text[2*x+3]))])
 	                   keyboard2=[]
 	                   for z in range(len(mes)//5):
 	                       keyboard2=[]
 	                       for yy in range(5):
-	                           keyboard2.append(InlineKeyboardButton(str(num),callback_data="Link"+str(text[2*x+2])+"_"+str(len(mes))+"_"+str(num)+"_"+str(text[2*x+1])))
+	                           keyboard2.append(InlineKeyboardButton(str(num),callback_data="Link"+str(text[2*x+4])+"_"+str(len(mes))+"_"+str(num)+"_"+str(text[2*x+3])))
 	                           num+=1
 	                       keyboard.append(keyboard2)
 	                   
 	                   print(str(keyboard))
 	                   keyboard2=[]
 	                   for z in range(len(mes)%5):
-	                       keyboard2.append(InlineKeyboardButton(str(num),callback_data="Link"+str(text[2*x+2])+"_"+str(len(mes))+"_"+str(num)+"_"+str(text[2*x+1])))
+	                       keyboard2.append(InlineKeyboardButton(str(num),callback_data="Link"+str(text[2*x+4])+"_"+str(len(mes))+"_"+str(num)+"_"+str(text[2*x+3])))
 	                       num+=1
 	                   keyboard.append(keyboard2+keyboard1)
 	                   print(str(keyboard))

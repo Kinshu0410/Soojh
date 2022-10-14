4af2914fe40bb830695b21054760608a95cb3f9b
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-14 16:37:05 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz1.py
+++ Quiz1.py
@@ -22,6 +22,18 @@ Send /start to initiate the conversation.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
+from pyrogram import Client, idle
+from pyrogram.types import Message as Mespy
+from pyrogram.types import InputMediaDocument
+from pyrogram.types import InlineKeyboardMarkup as ikm
+from pyrogram.types import InlineKeyboardButton as ikb
+from pyrogram import filters as filpy
+app = Client("my_live_bot",#session_string="BQDQx-MAAcKa6bmK3-vwhmKd0v3v4-SXoQ7PWIIqkl6-0j_96Gq6apAJ1vRFPUQrWwbcHoNLj0ouYgn5yCAvkT-BW8iE-1lYomM1VwAzEKHNuUVqTYrFDpCsAZE2ko1DudXnRUWz1SBkxk8f6JzrDS57sBmx2oEgUBXfiyGjJXJYn2KC-TsCao4Cbt-x6pE3LWwPprjAqsN6LHY2y2WA4QnQVagTclIrp5_Cc4FZbRnyNcL_MwQ-7hLF_5psbi4c1hXOYXYCnjx3KQ0Q--f0ISiGSt8h3xRu39RozHP1ANrB3pz4e_Unmoe8ad7hmhzwuil8uVqaV1qspejkWDo_3cyyadzV2QAAAAAqZYQtAA",
+bot_token="1431722823:AAHk_VOD0WgepQ1us7eucQm3UQRYacHzmQM",
+api_id="13682659",
+api_hash="b984d240c5258407ea911f042c9d75f6")
+
+
 import time
 import re as reaaa
 import json
@@ -2233,18 +2245,7 @@ def pdfc(update,context):
 	update.message.reply_text(KrutidevToUnicode.convert_to_unicode(x))
 	return PDF
 	
-from pyrogram import Client, idle
-from pyrogram.types import Message as Mespy
-from pyrogram.types import InputMediaDocument
-from pyrogram.types import InlineKeyboardMarkup as ikm
-from pyrogram.types import InlineKeyboardButton as ikb
-from pyrogram import filters as filpy
-app = Client("my_live_bot",#session_string="BQDQx-MAAcKa6bmK3-vwhmKd0v3v4-SXoQ7PWIIqkl6-0j_96Gq6apAJ1vRFPUQrWwbcHoNLj0ouYgn5yCAvkT-BW8iE-1lYomM1VwAzEKHNuUVqTYrFDpCsAZE2ko1DudXnRUWz1SBkxk8f6JzrDS57sBmx2oEgUBXfiyGjJXJYn2KC-TsCao4Cbt-x6pE3LWwPprjAqsN6LHY2y2WA4QnQVagTclIrp5_Cc4FZbRnyNcL_MwQ-7hLF_5psbi4c1hXOYXYCnjx3KQ0Q--f0ISiGSt8h3xRu39RozHP1ANrB3pz4e_Unmoe8ad7hmhzwuil8uVqaV1qspejkWDo_3cyyadzV2QAAAAAqZYQtAA",
-bot_token="1431722823:AAHk_VOD0WgepQ1us7eucQm3UQRYacHzmQM",
-api_id="13682659",
-api_hash="b984d240c5258407ea911f042c9d75f6")
-import re as reaaa
-import time
+
 
 
 zza=1

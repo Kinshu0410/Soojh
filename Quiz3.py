9f56172091beab9ec9bc6ade1ca495ea5cf9a90f
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-16 18:20:32 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Quiz3.py
+++ Quiz3.py
@@ -2409,7 +2409,12 @@ def button(update: Update, context: CallbackContext) -> None:
 	       except:
 	           mes=[]
 	       text1=reaaa.sub(" .*?$","",query.message.reply_markup.inline_keyboard[-1][0].text)
-	       text=reaaa.split("/",reaaa.sub("https\://telegram\.me/|\?comment\=1","",query.message.reply_markup.inline_keyboard[-1][0].url))
+	       text=reaaa.split("/",reaaa.sub("(https\://telegram\.me/(c/|))|\?thread\=\d{1,}","",query.message.reply_markup.inline_keyboard[-1][0].url))
+	       print(text)
+	       try:
+	           text[0]=int("-100"+text[0])
+	       except:
+	           pass
 	       #m =  app.get_discussion_message(chat_id = "@"+text[0],message_id = int(text[1]))
 	       n =str( app.get_discussion_replies_count(chat_id = "@"+text[0],message_id = int(text[1])))
 	       if (int(x[1])==len(mes) )& (text1==n):

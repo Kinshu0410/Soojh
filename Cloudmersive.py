724b8ee9cc43938ff3df3f3fbeaa03dd225d4e2d
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-17 11:07:04 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -36,6 +36,7 @@ scheduler = AsyncIOScheduler(timezone="Asia/kolkata")
 def from_user_id(data):
     async def func(_, c: Client, m: Message):
         try:
+            print(m.from_user)
             user_id=m.from_user.id
             return bool(user_id == data)
         except:

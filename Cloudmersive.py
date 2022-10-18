36ccd9f204caa2c8f7973e0497dd38abc28c7336
kinshusharm0412 <kinshusharm0412@gmail.com>
2022-10-18 11:30:38 +0530

    speed se push kiya jyada koi kaam nahi hai

--- Cloudmersive.py
+++ Cloudmersive.py
@@ -2128,11 +2128,11 @@ yx2=0
 def receive_poll(update, context):
     """On receiving polls, reply to it by a closed poll copying the received poll"""
     global yx2
-    yx2+=7
+    yx2+=5
     asyncio.sleep(yx2)
     actual_poll = update.effective_message.poll
     asyncio.run(polltotext(actual_poll,update,context))
-    yx2-=7
+    yx2-=5
     
    
 
